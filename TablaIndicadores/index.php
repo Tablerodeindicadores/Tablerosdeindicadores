6fe648e742f4e90f1efbdb0f00d6a77de725ae37
Enrique Hdz Murillo <quiquehe_98@hotmail.com>
2021-01-19 10:03:37 -0600

    ultima

--- TablaIndicadores/index.php
+++ TablaIndicadores/index.php
@@ -83,13 +83,6 @@ if(!empty($_SESSION['active']))
         </div>
         <br>
         <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br><br>
-        <br>
         <div class="footer-w3l">
             <p>&copy; 2021 Zapatería Torreón. All rights reserved | Design by <a href="https://pappos.mx/">pappos</a></p>
         </div>
